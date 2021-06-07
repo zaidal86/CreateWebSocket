0a330c240f2f4534398972c1c87a6bc80ce2efab
william <william-vivier@hotmail.fr>
2021-06-07 15:20:53 +0200

    initial commit

--- main.js
+++ main.js
@@ -11,8 +11,7 @@ wsServer.on('request', function (request) {
   const connection = request.accept(null, request.origin);
   connection.on('message', function (message) {
     console.log('Received Message:', message.utf8Data);
-    connection.sendUTF('Ta gueule !');
-    connection.sendUTF('Hi this is WebSocket server!');
+    connection.send('Hi this is WebSocket server!');
   });
   connection.on('close', function (reasonCode, description) {
     console.log('Client has disconnected.');
