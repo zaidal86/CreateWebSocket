f8e8aa9fd97e5dcef7407338a2a5908940d01657
william <william-vivier@hotmail.fr>
2021-06-07 14:58:32 +0200

    initial commit

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
-const net = require('net');
-
-const server = net.createServer((socket) => {
-  socket.on('data', (data) => {
-    console.log(data.toString());
-  });
-  socket.write('SERVER: Hello! This is server speaking.<br>');
-  socket.end('SERVER: Closing connection now.<br>');
-}).on('error', (err) => {
-  console.error(err);
+// Node.js WebSocket server script
+const http = require('http');
+const WebSocketServer = require('websocket').server;
+const server = http.createServer();
+server.listen(9898);
+const wsServer = new WebSocketServer({
+  httpServer: server
 });
-server.listen(9898, () => {
-  console.log('opened server on', server.address().port);
+wsServer.on('request', function (request) {
+  const connection = request.accept(null, request.origin);
+  connection.on('message', function (message) {
+    console.log('Received Message:', message.utf8Data);
+    connection.sendUTF('Hi this is WebSocket server!');
+  });
+  connection.on('close', function (reasonCode, description) {
+    console.log('Client has disconnected.');
+  });
 });
